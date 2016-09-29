b14f8c1a6734e7cd0202467cf5627fde993080ba
Varda <vardamohantech@gmail.com>
2016-09-29 15:23:31 +0530

    Create MaxBinaryGap.java

---
+++ MaxBinaryGap.java
@@ -0,0 +1,26 @@
+public class MaxBinaryGap{
+  
+  public static void main(String args[]){
+    int num = 105;
+    System.out.println("Maximum Binary Gap for number "+num+" is "+ getMaxBinaryGap(num));
+  }
+ 
+  public int getMaxBinaryGap(int num) {
+      int maxBinaryGap = 0;
+    	int rightMostBit = 0;
+    	int counter = -1;     
+    	while (num > 0) {		
+    		rightMostBit = num & 1;//get right significant bit
+    		num = num >> 1;//shift one bit right     
+    		if (rightMostBit == 0 && counter >= 0) {
+    			counter++;
+    		}     
+    		if (rightMostBit == 1) {
+    			maxBinaryGap = counter > maxBinaryGap ? counter : maxBinaryGap;
+    			counter = 0;
+    		}
+    	} 
+    	return maxBinaryGap;
+    }
+  
+}
